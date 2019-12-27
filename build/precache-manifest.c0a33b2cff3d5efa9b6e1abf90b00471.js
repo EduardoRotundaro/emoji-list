c79df8cf6c0bf0bf705807fcc0819836848d9090
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-12-27 18:12:10 -0300

    :alembic: build to test

---
+++ build/precache-manifest.c0a33b2cff3d5efa9b6e1abf90b00471.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0eacf37687d5a9a51a40b7dc7a95da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c287ae668a2ff155f7",
+    "url": "/static/css/2.2831ab12.chunk.css"
+  },
+  {
+    "revision": "27e354f1e235be65caa6",
+    "url": "/static/css/main.5f753eb0.chunk.css"
+  },
+  {
+    "revision": "28c287ae668a2ff155f7",
+    "url": "/static/js/2.34b70712.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.34b70712.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27e354f1e235be65caa6",
+    "url": "/static/js/main.40bf7334.chunk.js"
+  },
+  {
+    "revision": "5bee6b38c4c4aac6f9f2",
+    "url": "/static/js/runtime-main.f58c9b8d.js"
+  }
+]);
