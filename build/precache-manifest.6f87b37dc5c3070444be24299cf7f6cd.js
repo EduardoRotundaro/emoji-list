fd72938e36f61414e40d56a78ea83630e929db80
unknown <eduardo.rotundaro@olhonocarro.com.br>
2019-12-29 21:30:52 -0300

    :package: v1.0.0

---
+++ build/precache-manifest.6f87b37dc5c3070444be24299cf7f6cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55570c79fee683be9a5811ab0af3152",
+    "url": "/emoji-list/index.html"
+  },
+  {
+    "revision": "e6fd3a28d7d391c52702",
+    "url": "/emoji-list/static/css/2.2841eb14.chunk.css"
+  },
+  {
+    "revision": "0bdf01f7e149c715a044",
+    "url": "/emoji-list/static/css/main.608f6b00.chunk.css"
+  },
+  {
+    "revision": "e6fd3a28d7d391c52702",
+    "url": "/emoji-list/static/js/2.4120688a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/emoji-list/static/js/2.4120688a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bdf01f7e149c715a044",
+    "url": "/emoji-list/static/js/main.1f497ba7.chunk.js"
+  },
+  {
+    "revision": "4f51aa562332b2396cb9",
+    "url": "/emoji-list/static/js/runtime-main.7f859dcc.js"
+  }
+]);
