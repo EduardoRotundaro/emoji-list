2e001f33c46c92d3fe66c21c35a2c5a095fe9f85
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-05-25 23:09:19 -0300

    :lipstick: Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,21 +5,29 @@ import {StarIcon} from '../../assets/icons';
 
 function Footer() {
     return (
-        <footer>
-            <div>
-                <div>
-                    Feito por <a href="https://github.com/EduardoRotundaro" target="_blank" rel="noopener noreferrer">{AUTHOR}</a>
+        <footer className="footer">
+            <div className="block">
+                <div className="block__row">
+                    <div className="block__column--10">
+                        <div className="footer__author">
+                            Feito por <a href="https://github.com/EduardoRotundaro" target="_blank" rel="noopener noreferrer">{AUTHOR}</a>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <a href="https://github.com/EduardoRotundaro/emoji-list" target="_blank" rel="noopener noreferrer">
-                        <figure>
-                            <StarIcon />
-                        </figure>
-                        Github
-                    </a>
+                <div className="block__row">
+                    <div className="block__column--10">
+                        <a className="footer__repository" href="https://github.com/EduardoRotundaro/emoji-list" target="_blank" rel="noopener noreferrer">
+                            <figure>
+                                <StarIcon />
+                            </figure>
+                            Github
+                        </a>
+                    </div>
                 </div>
-                <div>
-                    <small>v{APP_VERSION}</small>
+                <div className="block__row">
+                    <div className="block__column--10">
+                        <small className="footer__version">v{APP_VERSION}</small>
+                    </div>
                 </div>
             </div>
         </footer>
