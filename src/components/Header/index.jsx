dc8128dfe23832f7456d7d626d04e55afcca63e0
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-05-25 21:30:26 -0300

    :lipstick: Header and navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
-import NavBar from '../NavBar';
+import {SmileyCoolIcon} from '../../assets/icons';
 
 function Header() {
     return (
-        <header>
-            <NavBar />
+        <header className="header">
+            <h1 className="header__title">
+                Emoji List
+                <figure className="header__logo"><SmileyCoolIcon /></figure>
+            </h1>
+            <nav className="header__navbar">
+                <ul className="menu">
+                    <li className="menu__item">
+                        <Link to="/emoji-list/activity">Activity</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/animals-nature">Animals / Nature</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/flags">Flags</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/food-drink">Food / Drink</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/objects">Objects</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/smileys-people">Smileys / People</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/symbols">Symbols</Link>
+                    </li>
+                    <li className="menu__item">
+                        <Link to="/emoji-list/travel-places">Travel / Places</Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 }
